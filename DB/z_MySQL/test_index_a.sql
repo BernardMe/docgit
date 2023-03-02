cf088fe86fde82463ea4940713ce644715d17f6f
wangzhuo <mt_cook@126.com>
2023-03-02 18:44:28 +0800

    add z_MySQL

---
+++ DB/z_MySQL/test_index_a.sql
@@ -0,0 +1,17 @@
+-- 创建表结构
+create table T(
+id int primary key,
+k int not null,
+name varchar(16),
+index (k)
+)engine=InnoDB;
+
+-- 插入测试数据
+insert into T(id,k) values(100,1),(200,2),(300,3),(500,5),(600,6);
+
+-- 如果语句是 select * from T where ID=500，即主键查询方式
+select * from T where ID=500;
+
+-- 如果语句是 select * from T where k=5，即普通索引查询方式
+select * from T where k=5;
+
